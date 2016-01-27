f73534be42e709abd2e71f055cab097a1f4b789f
guillaume <=>
2016-01-28 00:55:13 +0100

    remaniement

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function categorie($bdd){
 	$sqlimportcat = "(SELECT CAT1 as categories FROM VIDEOS) UNION (SELECT CAT2 FROM VIDEOS where CAT2 != '' AND CAT2 NOT IN (SELECT CAT1 FROM VIDEOS)) ORDER BY categories ASC";
 	$resultimportcat = mysqli_query($bdd,$sqlimportcat);
 	while($row = mysqli_fetch_array($resultimportcat)) {
-		echo "<li><a href='categorie.php?cat=".$row['categories']."'>".$row['categories']."</a></li>";
+		echo "<li><a href='categorie.php?cat=".$row['categories']."' title='".$row['categories']."'>".$row['categories']."</a></li>";
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -3,34 +3,34 @@ include("header.php");
 include("functions.php");
 ?>
 
-<div class="top-bar">
-  <div class="top-bar-title">
-    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-      <span class="menu-icon dark" data-toggle></span>
-    </span>
-  </div>
-  <div id="responsive-menu">
-    <div class="top-bar-left">
-      <ul class="dropdown menu" data-dropdown-menu>
-        <li><a href="#">home</a></li>
-        <li>
-          <a href="#">Catégories</a>
-          <ul class="menu vertical">
-            <?php
-            echo categorie($conn);
-            ?>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    <div class="top-bar-right">
-      <ul class="menu">
-        <li><input type="search" placeholder="Recherche"></li>
-        <li><button type="button" class="button step fi-magnifying-glass size-60"></button></li>
-      </ul>
-    </div>
-  </div>
-</div>
+<nav>
+	<!-- navigation mobile -->
+	<div class="title-bar" data-responsive-toggle="pornation-menu" data-hide-for="medium">
+		<button class="menu-icon" type="button" data-toggle></button>
+		<div class="title-bar-title">Menu</div>
+	</div>
+	<!-- navigation desktop -->
+	<div class="top-bar" id="pornation-menu">
+		<div class="top-bar-left">
+			<ul class="dropdown menu" data-dropdown-menu>
+				<li class="menu-text">Site Title</li>
+				<li><a href="http://www.pornation.fr" title="la nation du porno">Home</a></li>
+				<li>
+					<a href="#">Catégories</a>
+					<ul class="menu vertical">
+						<?php echo categorie($conn); ?>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="top-bar-right">
+			<ul class="menu">
+				<li><input type="search" placeholder="Recherche"></li>
+				<li><button type="button" class="button fi-magnifying-glass"></button></li>
+			</ul>
+		</div>
+	</div>
+</nav>
 
 <?php
 include("footer.php");
